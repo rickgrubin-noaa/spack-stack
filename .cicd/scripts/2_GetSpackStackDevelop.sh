439920a660d8c894e8e1777a5a49073eee48216f
Rick Grubin <Richard.Grubin@noaa.gov>
2024-08-30 16:44:38 -0500

    WIP

---
+++ .cicd/scripts/2_GetSpackStackDevelop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $SETUPDONE ]; then . ShellSetup.sh $* ; fi
+
+set -ex
+
+cd $RUNDIR
+if [ -d $RUNID ]; then
+  cd $RUNID
+  git pull
+else
+#  git clone --recurse-submodules https://github.com/AlexanderRichert-NOAA/spack-stack -b weekly_build $RUNID
+  git clone --recurse-submodules https://github.com/rickgrubin-noaa/spack-stack -b SI-1062 $RUNID
+fi
